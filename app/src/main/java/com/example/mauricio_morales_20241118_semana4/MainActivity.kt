3f1491630aba6db82476c02a80d48f402336a2f1
m4ury <mauriciomorales0410@gmail.com>
2024-11-20 13:37:50 -0300

    inicio

---
+++ app/src/main/java/com/example/mauricio_morales_20241118_semana4/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mauricio_morales_20241118_semana4
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, DetalleJugueteActivity::class.java)
+        intent.putExtra("nombre", juguete.nombre)
+        intent.putExtra("precio", juguete.precio)
+        intent.putExtra("imagen", juguete.imagenResId)
+        startActivity(intent)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
